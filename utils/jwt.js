3eda46d7b5b2d297ff0d95734980900724c9b8cf
Emma Clark <emmabossuytclark@gmail.com>
2025-10-01 17:33:04 -0300

    Added util folder and created jwt.js file

---
+++ utils/jwt.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+export function generateToken(user) {
+  return jwt.sign(
+    {
+      id: Number(user.id),
+      email: user.email,
+      firstname: user.firstname,
+      lastname: user.lastname,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "8h" }
+  );
+}
+
+export function verifyToken(token) {
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET);
+  } catch {
+    return null;
+  }
+}
