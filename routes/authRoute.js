68246c74bb901aa1fb6f7f212035311f4bb7e18e
Emma Clark <emmabossuytclark@gmail.com>
2025-10-01 17:32:26 -0300

    Added route to create a new user

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { createUser } from "../controllers/user/userController.js";
 const router = express.Router();
 
 router.get("/login", (req, res) => {
@@ -8,5 +9,7 @@ router.get("/login", (req, res) => {
 router.get("/signup", (req, res) => {
   res.render("provider/signup");
 });
+
+router.post("/signup", createUser);
 
 export default router;
