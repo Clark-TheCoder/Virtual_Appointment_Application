81a54d177b15bbc2379e231f263bb3393347ccd0
Emma Clark <emmabossuytclark@gmail.com>
2025-10-02 09:50:12 -0300

    Added authenticateUser function called when the user wants to login

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -48,6 +48,51 @@ export async function createUser(req, res) {
 
     return res.status(201).json({ message: "New user created successfully" });
   } catch (error) {
-    return res.status(500).json({ message: "Server error: " + error.message });
+    return res.status(500).json({ message: "Server error" });
+  }
+}
+
+export async function authenticateUser(req, res) {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "All fields are required to complete user authenication.",
+    });
+  }
+  try {
+    const existingUser = await findUserByEmail(email);
+    if (!existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Login failure. Please try again." });
+    }
+
+    const match = await bcrypt.compare(password, existingUser.password);
+    if (!match) {
+      return res
+        .status(400)
+        .json({ message: "Login failure. Please try again." });
+    }
+
+    // Remove password from user var that will be used in the jwt.js file
+    const user = {
+      id: existingUser.id,
+      email: existingUser.email,
+      firstname: existingUser.firstname,
+      lastname: existingUser.lastname,
+    };
+    const token = generateToken(user);
+
+    // Set cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Strict",
+      maxAge: 8 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({ message: "Login successful." });
+  } catch (error) {
+    return res.status(500).json({ message: "Server error" });
   }
 }
