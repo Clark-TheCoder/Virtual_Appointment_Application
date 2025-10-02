9f96adbcb8d78f337de92c29d840b8602aa400a5
Emma Clark <emmabossuytclark@gmail.com>
2025-10-02 14:08:14 -0300

    Added createNewCall function

--- models/call/callModel.js
+++ models/call/callModel.js
@@ -1,1 +1,23 @@
 import pool from "../db.js";
+
+export async function createNewCall(
+  call_id,
+  provider_id,
+  patient_alias,
+  access_token
+) {
+  try {
+    const [result] = await pool.execute(
+      `INSERT INTO calls (
+        call_id,
+        provider_id, 
+        patient_alias,
+        access_token
+      ) VALUES (?, ?, ?, ?)`,
+      [call_id, provider_id, patient_alias, access_token]
+    );
+    return result.affectedRows > 0;
+  } catch (error) {
+    throw new Error("Error creating call: " + error.message);
+  }
+}
