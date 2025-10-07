32e1fda3702b044be561e18bd1d412ed414a3e4b
Emma Clark <emmabossuytclark@gmail.com>
2025-10-07 19:42:12 -0300

    Added functionality on the visit form webage that if the page reloads, the user is sent back to the scheduled calls page and the session storage clears the access_token. Also called the getFromFunctionality function to activate the events and other functionality of the visit summary form

--- public/js/provider/visitSummary/index.js
+++ public/js/provider/visitSummary/index.js
@@ -1,6 +1,20 @@
 import { getCallData } from "../../api/fetchCallData.js";
+import { getFormFunctionality } from "./formFunctionality/formFunctionality.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-  console.log(sessionStorage.getItem("access_token"));
-  await getCallData(sessionStorage.getItem("access_token"));
+  // Get data
+  let access_token = sessionStorage.getItem("access_token");
+  let callData = await getCallData(access_token);
+
+  //Load data into the form
+  getFormFunctionality(callData);
+});
+
+// If the page is reloaded, send the user back to the main page
+window.addEventListener("load", () => {
+  const [navigation] = performance.getEntriesByType("navigation");
+  if (navigation.type === "reload") {
+    sessionStorage.removeItem("access_token");
+    window.location.href = "/call/scheduled_calls";
+  }
 });
