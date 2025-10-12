1c24ce8d1b8e03d34da9ea3f177d151464c9413b
Emma Clark <emmabossuytclark@gmail.com>
2025-10-12 14:03:27 -0300

    Added the startCall function and the updateCameraUIOff and updateAudioUIOff functions for then the startCall fetch is successful

--- public/js/provider/scheduledCalls/precall/buttons/joinCallButton.js
+++ public/js/provider/scheduledCalls/precall/buttons/joinCallButton.js
@@ -6,6 +6,9 @@ import {
   cameraStream,
   deactivateCamera,
 } from "../../../../media/cameraController.js";
+import { startCall } from "../../api/startCall.js";
+import { updateAudioUIOff } from "./audioButton.js";
+import { updateCameraUIOff } from "./cameraButton.js";
 
 export function createJoinButton() {
   const joinButton = document.getElementById("join_call_button");
@@ -14,15 +17,21 @@ export function createJoinButton() {
 
 async function joinCall() {
   // api calls
+  let isCallStarted = await startCall();
 
-  let callStarted = await startCall();
-
-  if (callStarted) {
+  if (isCallStarted) {
     if (cameraStream) {
       deactivateCamera();
+      // update the UI to show the camera is turned off (good if there are any delays)
+      updateCameraUIOff();
     }
     if (audioStream) {
       deactivateAudio();
+      // update the UI to show the audio is turned off (good if there are any delays)
+      updateAudioUIOff();
     }
+    window.location.href = "/incall";
+  } else {
+    alert("Could not begin call at this time.");
   }
 }
