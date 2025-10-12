f4b45773e4c1c63294abcda9304440d600bff4e8
Emma Clark <emmabossuytclark@gmail.com>
2025-10-12 09:20:31 -0300

    Added the showPreCallPopup() function when the join button on the call card is selected

--- public/js/provider/scheduledCalls/callCard/ui/buttons/createJoinButton.js
+++ public/js/provider/scheduledCalls/callCard/ui/buttons/createJoinButton.js
@@ -1,9 +1,12 @@
+import { showPreCallPopup } from "../../../precall/createPreCallPopup.js";
+
 export function createJoinButton(call) {
   const joinButton = document.createElement("button");
   joinButton.classList.add("button");
   joinButton.textContent = "Join Call";
   joinButton.addEventListener("click", () => {
     console.log("join");
+    showPreCallPopup();
   });
   return joinButton;
 }
