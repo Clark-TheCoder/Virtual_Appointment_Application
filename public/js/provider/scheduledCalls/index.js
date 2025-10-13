7251a2d00985eff64f4b919c464bce45e2cf1166
Emma Clark <emmabossuytclark@gmail.com>
2025-10-13 11:14:15 -0300

    Added pageshow eventlistener to clean up camera and audio streams. I have to do it on the page load and not when the user leaves the page to join the call becuase the deactivate functions toggle the sessionStorage which i need to stay consistent when the user navigates from the preCall popup to the inCall window

--- public/js/provider/scheduledCalls/index.js
+++ public/js/provider/scheduledCalls/index.js
@@ -1,5 +1,22 @@
+import { deactivateAudio, getAudio } from "../../media/audioController.js";
+import { deactivateCamera, getCamera } from "../../media/cameraController.js";
 import { loadScheduledCalls } from "./api/loadCalls.js";
+import { updateAudioUIOff } from "./precall/buttons/audioButton.js";
+import { updateCameraUIOff } from "./precall/buttons/cameraButton.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   await loadScheduledCalls();
+});
+
+// Clean up video and audio streams if there are any
+window.addEventListener("pageshow", () => {
+  if (getCamera()) {
+    deactivateCamera();
+    updateCameraUIOff();
+  }
+
+  if (getAudio) {
+    deactivateAudio();
+    updateAudioUIOff();
+  }
 });
