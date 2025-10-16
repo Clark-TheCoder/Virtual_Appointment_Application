95e62dec3adb6d6cc56a55fba9a53190f452b3bb
Emma Clark <emmabossuytclark@gmail.com>
2025-10-16 09:56:38 -0300

    Added createEndCallButton function to initButtons

--- public/js/provider/liveCall/callFunctionality/buttons/initButtons.js
+++ public/js/provider/liveCall/callFunctionality/buttons/initButtons.js
@@ -1,7 +1,9 @@
+import { createEndCallButton } from "./endCallButton.js";
 import { createAudioButton } from "./liveAudioButton.js";
 import { createCameraButton } from "./liveCameraButton.js";
 
 export function initButtons() {
   createCameraButton();
   createAudioButton();
+  createEndCallButton();
 }
