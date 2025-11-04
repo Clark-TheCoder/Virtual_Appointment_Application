43e65f2d367b34c2c92299eb76d1a79a2d2d1a43
Emma Clark <emmabossuytclark@gmail.com>
2025-11-04 18:00:23 -0400

    Added  showCameraStream(cameraStream); to update the ui

--- public/js/provider/liveCall/callFunctionality/buttons/liveCameraButton.js
+++ public/js/provider/liveCall/callFunctionality/buttons/liveCameraButton.js
@@ -23,6 +23,7 @@ export async function toggleCameraButton() {
   } else {
     const trackStatus = toggleCamera();
     if (trackStatus) {
+      showCameraStream(cameraStream);
       updateCameraUIOn();
     } else {
       updateCameraUIOff();
